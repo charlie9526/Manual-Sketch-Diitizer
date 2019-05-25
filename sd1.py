4443986a7192101d4df3189658d803a3bb80c393
Ashan Sandeepa <40732224+charlie9526@users.noreply.github.com>
2019-05-26 01:26:15 +0530

    Add files via upload

---
+++ sd1.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+# Normal routines
+#img = cv2.imread('ws.png')
+
+
+def detct_shapes(img):
+    #img = cv2.imread(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 50, 255, 1)
+
+    # Remove some small noise if any.
+    dilate = cv2.dilate(thresh, None)
+    erode = cv2.erode(dilate, None)
+
+    # Find contours with cv2.RETR_CCOMP
+    contours, hierarchy = cv2.findContours(
+        erode, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    image_subs = []
+    for i, cnt in enumerate(contours):
+        # Check if it is an external contour and its area is more than 100
+        if hierarchy[0, i, 3] == -1 and cv2.contourArea(cnt) > 100:
+            detail = []
+            x, y, w, h = cv2.boundingRect(cnt)
+            img_inst = img[y-10:y+h+10, x-10:(x+w+10)]
+            detail.append([[x-10, y-10], [x+w+10, y+h+10]])
+            detail.append(img_inst)
+            image_subs.append(detail)
+            cv2.rectangle(img, (x-10, y-10), (x+w+10, y+h+10),
+                          (255, 255, 255), 2)
+            #k = 'sofsqure'+str(x)+'.png'
+            cv2.imwrite("asa.jpg", img)
+            #m = cv2.moments(cnt)
+            #cx,cy = m['m10']/m['m00'],m['m01']/m['m00']
+            # cv2.circle(img,(int(cx),int(cy)),3,255,-1)
+    return image_subs
+
+#print detct_shapes(img)
+# cv2.imshow('img',img)
+# cv2.imwrite('sofsqure.png',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
